7c7847bf49b56b00b2f3488e07ee88508271d1b9
nitrxgen <46610988+nitrxgen@users.noreply.github.com>
2021-09-05 00:55:12 +0300

    Update lm_hash.php
    Added the main function contents.

--- lm_hash.php
+++ lm_hash.php
@@ -1,5 +1,16 @@
 <?php
 
+function lm_hash(string $input = '', bool $double = true): string {
+  if($double)
+    return call_user_func(__FUNCTION__, substr($input, 0, 7), false).
+           call_user_func(__FUNCTION__, substr($input, 7, 7), false);
 
+  $input = unpack('C*', strtoupper($input));
+
+  for($i=0,$key=chr(@$input[1]&254);$i<7;$i++)
+    $key.= chr((@$input[$i+1]<<7-$i)|(@$input[$i+2]>>$i+1));
+
+  return bin2hex(openssl_encrypt('KGS!@#$%', 'des-ecb', $key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING));
+}
 
 ?>
